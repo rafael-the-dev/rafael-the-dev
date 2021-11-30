a28aac74f1ca1566332a638e75f1c5c8f38c62fd
Rafael Tivane <luislangabusiness@gmail.com>
2021-11-30 13:03:07 +0200

    adding search-form

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,6 +1,6 @@
 import { Button, Grid, Typography } from '@mui/material';
 import classNames from 'classnames';
-import { useBackground, useDisplay, useTypography } from '../../styles';
+import { useBackground, useDisplay, useResponsive, useTypography } from '../../styles';
 import { useStyles } from './styles';
 import { Link } from 'react-router-dom';
 //import audiophileImage from '../../assets/images/audiophile.jpg'
@@ -11,18 +11,24 @@ import blueCoffeeIcon from '../../assets/images/coffee-cup.png'*/
 import { useContext } from 'react';
 import { AppContext } from '../../context/AppContext'
 import Card from './Card';
+import SearchIcon from '@mui/icons-material/Search';
 
 const ProjectsContainer = () => {
     const display = useDisplay();
     const text = useTypography();
     const bg = useBackground();
+    const responsive = useResponsive();
     const classes = useStyles();
 
     const { projects } = useContext(AppContext)
 
     return (
        <main className={classNames(display.px5)}>
-           <Grid container className={classNames(display.mt2)}>
+           <form className={classNames(display.flex, display.alignStretch, display.w100, classes.searchForm, display.mb3)}>
+               <input className={classNames(display.flexGrow1, display.borderNone, display.outlineNone, classes.searchInput)} />
+               <Button endIcon={<SearchIcon />} className={classNames(display.outlineNone, classes.searchButton)}>Search</Button>
+           </form>
+           <Grid container className={classNames(display.mt2, responsive.mdMt2)}>
                {
                    Object.entries(projects).map((project, index) => (
                        <Card nameParameter={project[0]} project={project[1]} key={index}/>
--- src/pages/Projects/styles.js
+++ src/pages/Projects/styles.js
@@ -1,4 +1,21 @@
 import { makeStyles } from "@mui/styles";
 
 export const useStyles = makeStyles(theme => ({
+    searchForm: {
+        border: '1px solid #CCC',
+        [theme.breakpoints.up(800)]: {
+            marginLeft: '15%',
+            marginRight: '15%',
+            width: '70%'
+        }
+    },
+    searchInput: {
+        color: '#555262',
+        fontFamily: "'Roboto', sans-serif",
+        fontSize: '1rem',
+        paddingLeft: 10
+    },
+    searchButton: {
+        height: '53px !important'
+    }
 }));
