a603e989b9bf4bde6b8da63dad11af01fb9558e8
Rafael Tivane <luislangabusiness@gmail.com>
2022-01-21 17:39:45 +0200

    adding buttons, transitions and toggle events

--- src/components/ColorSwitcher/index.jsx
+++ src/components/ColorSwitcher/index.jsx
@@ -3,15 +3,26 @@ import classNames from 'classnames';
 import { useDisplay, useTypography } from '../../styles';
 import spinnerImage from '../../assets/images/spinner-image.png';
 import './styles.scss'
+import { useCallback, useState } from 'react';
 
 const ColorSwitcher = () => {
     const classes = useStyles();
     const display = useDisplay();
 
+    const [ open, setOpen ] = useState(false);
+    const openHandler = useCallback(() => setOpen(true), []);
+    const closeHandler = useCallback(() => setOpen(false), []);
+
     return (
         <div className={classNames(classes.container)}>
-            <button className={classNames(classes.spinner, display.borderNone, display.outlineNone, 
-            display.relative, 'spinner')} aria-label="color switcher"></button>
+            <div className={classNames(display.relative)}>
+            <button onClick={openHandler} className={classNames(classes.spinner, display.borderNone, display.outlineNone, 
+                display.relative, 'spinner', {[classes.spinnerHide]: open})} aria-label="color switcher"></button>
+                <div className={classNames(classes.buttonsContainer, {[classes.buttonsContainerDisplay]: open})}>
+                    <button onClick={closeHandler}  className={classNames(classes.blueTheme, display.borderNone, display.outlineNone)}></button>
+                    <button onClick={closeHandler}  className={classNames(classes.lightPinkTheme, display.borderNone, display.outlineNone)}></button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/ColorSwitcher/styles.js
+++ src/components/ColorSwitcher/styles.js
@@ -4,11 +4,38 @@ export const useStyles = makeStyles(theme => ({
     container: {
         left: 0,
         position: 'fixed',
-        top: '48vh'
+        top: '48vh',
+        zIndex: 1000
     },
     spinner: {
         borderRadius: '50%',
         height: 50,
+        transition: 'left 2s ease-in-out',
         width: 50
+    },
+    spinnerHide: {
+        left: -200,
+        position: 'absolute',
+    },
+    buttonsContainer: {
+        background: '#FFF',
+        display: 'grid',
+        gridGap: '10px 10px',
+        gridTemplateColumns: '40px 40px',
+        gridTemplateRows: '40px',
+        left: -250,
+        padding:' 0.6rem 0.7rem',
+        position: 'absolute',
+        transition: 'left 2s ease-in-out',
+        top: 0
+    },
+    buttonsContainerDisplay: {
+        left: 0
+    },
+    blueTheme: {
+        backgroundColor: '#1976d2'
+    },
+    lightPinkTheme: {
+        backgroundColor: '#E7816B'
     }
 }));
